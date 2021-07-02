02ca9e2550850551a6896f4f9768ccce813da2d7
Bidan <bydangitau@gmail.com>
2021-07-02 15:38:35 +0300

    f

--- duplicate.js
+++ duplicate.js
@@ -1,21 +1,3 @@
-// const findDuplicate=(arr)=>{
-//     const noneDupAdj=[]
-  
-//     const arrString=arr.split('');
-//     for (let i = 0; i < arrString.length; ++i) {
-//         if (arrString == arrString[i + 1]) continue
-            
-//            return noneDupAdj.push(arrString[i])
-       
-//       } 
-      
-// }
-
-
-
-// console.log(findDuplicate('abbaca'))
-
-
 const findDuplicate=(arr)=>{
     let noneDupAdj=[];
     const lowercase=arr.toLowerCase();
@@ -37,4 +19,47 @@ const findDuplicate=(arr)=>{
       return noneDupAdj.join(""); 
 }
 console.log(findDuplicate('cKKaaeel'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const findDuplicate=(arr)=>{
+//     const noneDupAdj=[]
+  
+//     const arrString=arr.split('');
+//     for (let i = 0; i < arrString.length; ++i) {
+//         if (arrString == arrString[i + 1]) continue
+            
+//            return noneDupAdj.push(arrString[i])
+       
+//       } 
+      
+// }
+
+
+
+// console.log(findDuplicate('abbaca'))
 
