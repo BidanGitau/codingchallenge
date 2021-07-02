32cd2b556d285f305f73006e084e1f731d6deb58
Bidan <bydangitau@gmail.com>
2021-07-02 13:45:02 +0300

    coding challenge

---
+++ duplicate.js
@@ -0,0 +1,41 @@
+// const findDuplicate=(arr)=>{
+//     const noneDupAdj=[]
+  
+//     const arrString=arr.split('');
+//     for (let i = 0; i < arrString.length; ++i) {
+//         if (arrString == arrString[i + 1]) continue
+            
+//            return noneDupAdj.push(arrString[i])
+       
+//       } 
+      
+// }
+
+
+
+// console.log(findDuplicate('abbaca'))
+// const findDuplicate=(arr)=>{
+//     let noneDupAdj=[];
+//     const arrString=arr.split('');
+//     for (let i = 0; i < arrString.length; i++) {
+//         if (arrString[i] == noneDupAdj[noneDupAdj-1]) {
+//          noneDupAdj.pop()
+//         }else{
+//           noneDupAdj.push(arrString[i])
+//         }
+//        return noneDupAdj.join("");
+//       } 
+// }
+// console.log(findDuplicate('abbaca'))
+
+function removeDuplicateCharacters(string) {
+    return string
+      .split('')
+      .filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+      })
+      .join('');
+  }
+  console.log(removeDuplicateCharacters('abbaca'));
+
+ 
